23bd54972a76eb0e7d535fedf25329394ebfe3fc
JonathanLoza <lozajnt@gmail.com>
2019-11-20 20:03:46 -0500

    Add project files

---
+++ app/src/main/java/com/example/indoor_location/IndoorLocationActivity.java
@@ -0,0 +1,13 @@
+package com.example.indoor_location;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndoorLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indoor_location);
+    }
+}
---
+++ app/src/main/java/com/example/indoor_location/LocationBottomSheet.java
@@ -0,0 +1,4 @@
+package com.example.indoor_location;
+
+public class LocationBottomSheet {
+}
